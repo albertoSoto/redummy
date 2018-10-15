ddf41a0f258dbd6b55bae53319aad30b25802105
Alberto Soto <alberto.soto@gmail.com>
2018-10-15 17:14:49 +0200

    exception/IESMyException.java

--- src/main/java/com/iesemilidarder/asoto/exception/IESMyException.java
+++ src/main/java/com/iesemilidarder/asoto/exception/IESMyException.java
@@ -10,7 +10,7 @@ public class IESMyException extends RuntimeException {
     public IESMyException(){
         super();
     }
-    
+
     public IESMyException(String s) {
         super(s);
     }
